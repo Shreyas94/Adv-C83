f0cbd3ccae31722bd65f65bd333ef110395143dc
Shreyas <72331003+Shreyas94@users.noreply.github.com>
2020-12-06 11:52:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ if (width < 992) {
     document.getElementById("myCanvas").height = new_height;
     document.body.style.overflow = "hidden";
 }
-
 canvas.addEventListener("touchstart", my_touchstart);
 
 function my_touchstart(e) {
@@ -22,30 +21,27 @@ function my_touchstart(e) {
     //Addictonal Activity ends
     last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
     last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
-
 }
-
-
 canvas.addEventListener("touchmove", my_touchmove);
 
 function my_touchmove(e) {
     console.log("my_touchmove");
     current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
     current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
-
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.lineWidth = width_of_line;
-
     console.log("Last position of x and y coordinates = ");
     console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
     ctx.moveTo(last_position_of_x, last_position_of_y);
-
     console.log("Current position of x and y coordinates = ");
     console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
     ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
     ctx.stroke();
-
     last_position_of_x = current_position_of_touch_x;
     last_position_of_y = current_position_of_touch_y;
+}
+
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
